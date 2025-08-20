a7a36a3febbadd80d727ae4e700ded64c02a8945
Walidelmasri <waldaexist@gmail.com>
2025-08-20 11:54:57 +0300

    Pillars ok

--- obj/Debug/net9.0/KPIMonitor.AssemblyInfo.cs
+++ obj/Debug/net9.0/KPIMonitor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KPIMonitor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+824ce69cc16f143e2c9fe3a1ec6686f8f7224596")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86df42e670bdf5d44c852d0dea078a3e890b8e24")]
 [assembly: System.Reflection.AssemblyProductAttribute("KPIMonitor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KPIMonitor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
