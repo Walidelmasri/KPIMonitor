0bf8aded898d1051f57fe89d27433e7ff0d0cfce
Walidelmasri <waldaexist@gmail.com>
2025-08-25 12:45:21 +0300

    Changed title for redboard

--- obj/Debug/net9.0/KPIMonitor.AssemblyInfo.cs
+++ obj/Debug/net9.0/KPIMonitor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KPIMonitor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cc9b9a3d1df8369c1b03078de25a0d9204d7815")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31537bddfc7c4027f9421bf50b42d42c15ec1fb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("KPIMonitor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KPIMonitor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
