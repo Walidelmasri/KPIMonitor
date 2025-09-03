5db46a51f2c9d5c879af2f3e29818a1131a94f76
Walidelmasri <waldaexist@gmail.com>
2025-09-03 11:14:03 +0300

    Edited how to page

--- obj/Debug/net9.0/KPIMonitor.AssemblyInfo.cs
+++ obj/Debug/net9.0/KPIMonitor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KPIMonitor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a1db4027b8a40eeddb068cce1f2eecdc2bdbdc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdfa7e673cbfeea44f0c6eb321fec5d96c4ac257")]
 [assembly: System.Reflection.AssemblyProductAttribute("KPIMonitor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KPIMonitor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
