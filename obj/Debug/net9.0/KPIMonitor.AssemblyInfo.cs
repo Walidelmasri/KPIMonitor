af0f2a9803f6dc2851ae1150da1a509c521898f5
Walidelmasri <waldaexist@gmail.com>
2025-09-07 08:26:52 +0300

    changed redboard chart

--- obj/Debug/net9.0/KPIMonitor.AssemblyInfo.cs
+++ obj/Debug/net9.0/KPIMonitor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KPIMonitor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf255a7227cc66c97eda658f77d9097e01179e46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50d8ae80da6cb385970519055131d276292a08f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("KPIMonitor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KPIMonitor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
