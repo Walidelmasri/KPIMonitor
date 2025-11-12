721a93c5ca1e50777c2ce5a21b79648d4c3fa64a
Walidelmasri <waldaexist@gmail.com>
2025-11-12 14:21:09 +0300

    final fix hopefully

--- obj/Debug/net9.0/KPIMonitor.AssemblyInfo.cs
+++ obj/Debug/net9.0/KPIMonitor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KPIMonitor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffd2f0cc681b9e7afe1e3dff7df2faa1500572d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67589f6b7c643da985fd3d9088a63f86757e4fad")]
 [assembly: System.Reflection.AssemblyProductAttribute("KPIMonitor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KPIMonitor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
