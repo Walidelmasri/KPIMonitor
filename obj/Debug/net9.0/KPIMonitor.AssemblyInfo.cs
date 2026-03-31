c131067d4c0cb099e761fcfaaddf3128d3587a11
Walidelmasri <waldaexist@gmail.com>
2026-03-31 11:29:23 +0300

    fix modal for kpi edit

--- obj/Debug/net9.0/KPIMonitor.AssemblyInfo.cs
+++ obj/Debug/net9.0/KPIMonitor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KPIMonitor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fe8161e29c80745e3849cf78c6ca3da169dcc55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e81c57edd74a8e94902299a3f1f62625dc4b041")]
 [assembly: System.Reflection.AssemblyProductAttribute("KPIMonitor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KPIMonitor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
