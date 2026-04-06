faf7ab0bd6d08ef4941064e20ab3882499d5c67a
Walidelmasri <waldaexist@gmail.com>
2026-04-06 10:53:11 +0300

    homepage translated

--- obj/Debug/net9.0/KPIMonitor.AssemblyInfo.cs
+++ obj/Debug/net9.0/KPIMonitor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KPIMonitor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+040a3a25abe84a38f626a6cc3fbffabfd303e8fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ec090ac994ca6bf7a6e9f46c3ddc2fd5e7786e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("KPIMonitor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KPIMonitor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
